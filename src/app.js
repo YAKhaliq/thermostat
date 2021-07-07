1910676d6271984893ae07591fa2690aae75920f
Yusuf <yakhaliq01@gmail.com>
2021-07-07 16:04:47 +0100

    added gitignore

--- src/app.js
+++ src/app.js
@@ -46,3 +46,22 @@ reset.addEventListener('click', () =>{
 let powerUsage = document.getElementById("energy-usage")
 powerUsage.innerHTML = thermostat.currentEnergyUsage();
 
+const displayWeather = (city) => {
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric`
+
+  fetch(url)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      document.querySelector('#current-temperature').innerText = data.main.temp;
+    })
+}
+displayWeather('London');
+
+document.querySelector('#select-city').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const city = document.querySelector('#current-city').value;
+
+  displayWeather(city);
+})
